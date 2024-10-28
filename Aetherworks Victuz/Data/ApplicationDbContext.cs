686f907373afba065f61fccaa72e917ada414574
Ravi <150629030+TheRealSpaceBaker@users.noreply.github.com>
2024-10-28 11:58:40 +0100

    Add project files.

---
+++ Aetherworks Victuz/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aetherworks_Victuz.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Aetherworks Victuz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aetherworks_Victuz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
