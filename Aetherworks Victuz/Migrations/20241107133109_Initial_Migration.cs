71cc58389633fbdf6e72b1bb8aceca79cf171458
Ravi <150629030+TheRealSpaceBaker@users.noreply.github.com>
2024-11-07 14:34:52 +0100

    Updated Database (#60)
    * Add NormalizedName to roles and update image file
    
    The ApplicationDbContext.cs file now includes the NormalizedName property for IdentityRole entities, ensuring case-insensitive lookups for role names.
    
    The e8134e3c-52a2-4764-9388-6204ba60db81.jpg file has been modified, likely indicating an update or replacement of the image content.
    
    * Updated Migration

--- Aetherworks Victuz/Migrations/20241107133109_Initial_Migration.cs
+++ Aetherworks Victuz/Migrations/20241107133109_Initial_Migration.cs
@@ -339,9 +339,9 @@ namespace Aetherworks_Victuz.Migrations
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
                 values: new object[,]
                 {
-                    { "1b5b8cdf-e551-4cd5-9812-f89ccb4f58fd", null, "Organizer", null },
-                    { "4df2b64b-1533-447f-b8de-3ee692790db1", null, "Guest", null },
-                    { "6e39927d-1081-468b-b60a-fedfe1549d0f", null, "Member", null }
+                    { "2d1875c1-6bb3-4214-8689-667f075009e4", null, "Member", "MEMBER" },
+                    { "42537b5b-cea4-4fc4-b29c-dd7629a5c752", null, "Organizer", "ORGANIZER" },
+                    { "644ce47f-e92e-4f12-b836-d277fd370c87", null, "Guest", "GUEST" }
                 });
 
             migrationBuilder.InsertData(
@@ -349,10 +349,10 @@ namespace Aetherworks_Victuz.Migrations
                 columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
                 values: new object[,]
                 {
-                    { "15e27e09-4b3e-4934-b227-09e44fef3219", 0, "6f44b994-4920-49ff-84a3-37edfc164be6", "member@gmail.com", true, true, null, "MEMBER@GMAIL.COM", "MEMBER", "AQAAAAIAAYagAAAAEO/MrnGzjJfNjh+vU2Zv9Dv1TR4ZFhiYqBkKFPYFFSVIT+S4DNyYqlNlFb/+ba/vjw==", null, false, "LFSRBIXYR4P6ZTHPXRWDIQ7M5GTLJXK7", false, "Member" },
-                    { "25335a7f-df48-4b77-8629-468a2015c0b5", 0, "bf636d49-9342-4af5-aa7b-b1e9dd4a3a10", "guest@gmail.com", true, true, null, "GUEST@GMAIL.COM", "GUEST", "AQAAAAIAAYagAAAAEC9Tmh0HNHm5EQL0YPRmTJTZRmjRX4OnzusW767S7O2uW5XKJov6oSZPrQx/RGEcRA==", null, false, "RQLSCP23C4O43IDZW3SETEUO2GI7VZOP", false, "Guest" },
-                    { "35cbcaf4-d21b-49e3-a496-2c3562738521", 0, "cf94f079-f33c-42fb-be66-f15601fdb549", "lock@gmail.com", true, true, new DateTimeOffset(new DateTime(2124, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)), "LOCK@GMAIL.COM", "LOCK", "AQAAAAIAAYagAAAAEBfVTJQI47BIH3mpec90MDkrbH8RxtAFRnFQ6UFD2pY5hY3ABSze9cDHDd88b5E2Pg==", null, false, "6IS3TH2BMU4QPOKYPZNBUE3WJFM6R5MO", false, "Lock" },
-                    { "6f051aeb-b4ad-4cff-87db-0566f9c7474e", 0, "3d08f465-c409-412d-85c1-f4a212fc2e25", "organizer@gmail.com", true, true, null, "ORGANIZER@GMAIL.COM", "ORGANIZER", "AQAAAAIAAYagAAAAEBCO7kfhleA+rJgzblvMlQh/8EzLDeKO1hRDHFxuAX4hRaLAOZEICsYhYKoI97QYew==", null, false, "MRKIS7ZM3PEX7XJX7FGMPZY4NKTH6Z76", false, "Organizer" }
+                    { "074aa6fb-9351-4efc-9a0b-7e7be1a844c3", 0, "bf636d49-9342-4af5-aa7b-b1e9dd4a3a10", "guest@gmail.com", true, true, null, "GUEST@GMAIL.COM", "GUEST", "AQAAAAIAAYagAAAAEC9Tmh0HNHm5EQL0YPRmTJTZRmjRX4OnzusW767S7O2uW5XKJov6oSZPrQx/RGEcRA==", null, false, "RQLSCP23C4O43IDZW3SETEUO2GI7VZOP", false, "Guest" },
+                    { "29b452f8-7be2-43da-9b7b-66314b048544", 0, "cf94f079-f33c-42fb-be66-f15601fdb549", "lock@gmail.com", true, true, new DateTimeOffset(new DateTime(2124, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0)), "LOCK@GMAIL.COM", "LOCK", "AQAAAAIAAYagAAAAEBfVTJQI47BIH3mpec90MDkrbH8RxtAFRnFQ6UFD2pY5hY3ABSze9cDHDd88b5E2Pg==", null, false, "6IS3TH2BMU4QPOKYPZNBUE3WJFM6R5MO", false, "Lock" },
+                    { "a3b14bfb-aa9d-4c0e-bed0-4d1dc03df342", 0, "3d08f465-c409-412d-85c1-f4a212fc2e25", "organizer@gmail.com", true, true, null, "ORGANIZER@GMAIL.COM", "ORGANIZER", "AQAAAAIAAYagAAAAEBCO7kfhleA+rJgzblvMlQh/8EzLDeKO1hRDHFxuAX4hRaLAOZEICsYhYKoI97QYew==", null, false, "MRKIS7ZM3PEX7XJX7FGMPZY4NKTH6Z76", false, "Organizer" },
+                    { "ce99840f-75a9-454b-818a-fe19e1d74724", 0, "6f44b994-4920-49ff-84a3-37edfc164be6", "member@gmail.com", true, true, null, "MEMBER@GMAIL.COM", "MEMBER", "AQAAAAIAAYagAAAAEO/MrnGzjJfNjh+vU2Zv9Dv1TR4ZFhiYqBkKFPYFFSVIT+S4DNyYqlNlFb/+ba/vjw==", null, false, "LFSRBIXYR4P6ZTHPXRWDIQ7M5GTLJXK7", false, "Member" }
                 });
 
             migrationBuilder.InsertData(
@@ -381,10 +381,10 @@ namespace Aetherworks_Victuz.Migrations
                 columns: new[] { "RoleId", "UserId" },
                 values: new object[,]
                 {
-                    { "6e39927d-1081-468b-b60a-fedfe1549d0f", "15e27e09-4b3e-4934-b227-09e44fef3219" },
-                    { "4df2b64b-1533-447f-b8de-3ee692790db1", "25335a7f-df48-4b77-8629-468a2015c0b5" },
-                    { "4df2b64b-1533-447f-b8de-3ee692790db1", "35cbcaf4-d21b-49e3-a496-2c3562738521" },
-                    { "1b5b8cdf-e551-4cd5-9812-f89ccb4f58fd", "6f051aeb-b4ad-4cff-87db-0566f9c7474e" }
+                    { "644ce47f-e92e-4f12-b836-d277fd370c87", "074aa6fb-9351-4efc-9a0b-7e7be1a844c3" },
+                    { "644ce47f-e92e-4f12-b836-d277fd370c87", "29b452f8-7be2-43da-9b7b-66314b048544" },
+                    { "42537b5b-cea4-4fc4-b29c-dd7629a5c752", "a3b14bfb-aa9d-4c0e-bed0-4d1dc03df342" },
+                    { "2d1875c1-6bb3-4214-8689-667f075009e4", "ce99840f-75a9-454b-818a-fe19e1d74724" }
                 });
 
             migrationBuilder.InsertData(
@@ -392,16 +392,16 @@ namespace Aetherworks_Victuz.Migrations
                 columns: new[] { "Id", "CredentialId" },
                 values: new object[,]
                 {
-                    { 1, "6f051aeb-b4ad-4cff-87db-0566f9c7474e" },
-                    { 2, "15e27e09-4b3e-4934-b227-09e44fef3219" },
-                    { 3, "25335a7f-df48-4b77-8629-468a2015c0b5" },
-                    { 4, "35cbcaf4-d21b-49e3-a496-2c3562738521" }
+                    { 1, "a3b14bfb-aa9d-4c0e-bed0-4d1dc03df342" },
+                    { 2, "ce99840f-75a9-454b-818a-fe19e1d74724" },
+                    { 3, "074aa6fb-9351-4efc-9a0b-7e7be1a844c3" },
+                    { 4, "29b452f8-7be2-43da-9b7b-66314b048544" }
                 });
 
             migrationBuilder.InsertData(
                 table: "Penalties",
                 columns: new[] { "Id", "EndDate", "RoleId", "UserId" },
-                values: new object[] { 1, new DateTime(2024, 10, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "4df2b64b-1533-447f-b8de-3ee692790db1", 2 });
+                values: new object[] { 1, new DateTime(2024, 10, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "644ce47f-e92e-4f12-b836-d277fd370c87", 2 });
 
             migrationBuilder.InsertData(
                 table: "Suggestions",
